00acc8b6f0905abb091647aca985108f76af9f42
Yevgeniy Shchemelev <shchemelevev@gmail.com>
2013-09-16 21:36:57 +0600

    Солдат начал двигаться

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ var BigGun = function(x, y, angle) {
     stage.addChild(sprite);
 
 
-    sprite.interactive = true;
+    sprite.interactive = true; // Почему не setInteractive?
 
     var drag = false;
     sprite.mousedown = function() {
@@ -133,35 +133,107 @@ BigGun.prototype = {
 };
 
 
-var Soldier = function() {
+var Soldier = function(x, y, angle) {
+    var it = this;
     var sprite = new PIXI.Sprite(textures_static.soldier);
     sprite.anchor.x = 0.5;
     sprite.anchor.y = 0.5;
-    sprite.position.x = 50;
-    sprite.position.y = 50;
+    sprite.position.x = x;
+    sprite.position.y = y;
+
+    sprite.rotation = angle.toRad();
+
+    sprite.setInteractive(true);
+
+    sprite.mousemove = function(mouseData){
+        if (it.is_mouse_down){
+            data = mouseData.global.clone();
+            it.dots.push(data);
+        }
+    }
+
+    sprite.mousedown = function(mouseData){
+        it.is_mouse_down = true;
+        it.dots = [];
+    }
+
+    sprite.mouseupoutside = function(mouseData){
+        it.is_mouse_down = false;
+        it.current_dot_index = 0;
+        it.dots = it.normalize_path();
+        this.start_time = new Date().getTime();
+        animation.pushToRender(it);
+    }
+
     stage.addChild(sprite);
+    this.angle = angle;
     this.sprite = sprite;
-    animation.pushToRender(this);
 };
 
 Soldier.prototype = {
-    dots: [
-        {x: 50, y: 50},
-        {x: 80, y: 70},
-        {x: 60, y: 150},
+    dots: [],
+    current_dot_index: 0,
+    is_mouse_down: false,
+    get_line_points: function line(x0, y0, x1, y1){
+        // source: http://stackoverflow.com/questions/4672279/bresenham-algorithm-in-javascript
+        // Работает плохо, линия не плавная
+        var line_dots = new Array();
+        var dx = Math.abs(x1-x0);
+        var dy = Math.abs(y1-y0);
+        var sx = (x0 < x1) ? 1 : -1;
+        var sy = (y0 < y1) ? 1 : -1;
+        var err = dx-dy;
 
-    ],
+        while(true){
+          if ((x0==x1) && (y0==y1)) {
+              return line_dots;
+          }
+          var e2 = 2*err;
+          if (e2 >-dy){ err -= dy; x0  += sx; }
+          if (e2 < dx){ err += dx; y0  += sy; }
+          line_dots.push({x:x0, y:y0})
+        }
+    },
+    normalize_path: function(){
+        // Заполняет пробелы в пути
+        // Нужно еще удалить дубли
+        var result_path = new Array();
+        var current_index = 0;
+        while (current_index + 1 < this.dots.length){
+            result_path.push(this.dots[current_index]);
+            if ( (Math.abs((this.dots[current_index].x-this.dots[current_index+1].x))>1) ||
+                 (Math.abs((this.dots[current_index].y-this.dots[current_index+1].y))>1) ){
+                result_path = result_path.concat(this.get_line_points(this.dots[current_index].x, 
+                                                         this.dots[current_index].y,
+                                                         this.dots[current_index+1].x,
+                                                         this.dots[current_index+1].y));
+            };
+            current_index += 1;
+        };
+        return result_path;
+    },
     renderRun: function() {
+        if (this.current_dot_index < this.dots.length){
+            
+            var dx = this.sprite.position.x - this.dots[this.current_dot_index].x;
+            var dy = this.sprite.position.y - this.dots[this.current_dot_index].y;
 
+            this.sprite.rotation = Math.tan(dx, dy) + this.angle.toRad(-90);
+
+            this.sprite.position.x = this.dots[this.current_dot_index].x;
+            this.sprite.position.y = this.dots[this.current_dot_index].y;
+            this.current_dot_index += 1;
+        }else{
+            animation.removeFromRender(this);
+        }
     },
     render: function() {
-        //this.sprite.position.x += 1;
-    }
+        this.renderRun();
+    },
 }
 
 $(document).ready(function() {
     var big_gun1 = new BigGun(70, $(window).height()/2, 270);
     var big_gun2 = new BigGun($(window).width()-70, $(window).height()/2, 90);
-    var soldier1 = new Soldier();
+    var soldier1 = new Soldier(50, 50, 90);
 });
-
